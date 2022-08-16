0ec11eb9522f1d6f435da5708fe08771a23b3ac0
ketan prajapati <109566171+ketanprajapati9531@users.noreply.github.com>
2022-08-16 12:14:23 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Cards';
+import "./index.css";
+
+
+
+
+ReactDOM.render(
+<>
+
+<h1 className='heading_style'> list of top 12 Netfix in 2018-2022 hindi</h1>
+
+<Card imgsrc="https://m.media-amazon.com/images/M/MV5BNzkxZjBjNGItMDk0MC00NzJjLTk2ZDItNTNkYWFkMDY4MzY1XkEyXkFqcGdeQXVyNDAzNDk0MTQ@._V1_.jpg" 
+      title="A Netfix Original Series" 
+      sname="Kota factory" 
+      link="https://www.netflix.com/in/title/81249783?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp5993832.png" 
+      title="A Netfix Original Series" 
+      sname="Money heist" 
+      link="https://www.netflix.com/in/title/80192098?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp3814512.png" 
+      title="A Netfix Original Series" 
+      sname="Sacred games" 
+      link="https://www.netflix.com/in/title/80115328?source=35" /> 
+
+
+<Card imgsrc="https://m.media-amazon.com/images/M/MV5BNzkxZjBjNGItMDk0MC00NzJjLTk2ZDItNTNkYWFkMDY4MzY1XkEyXkFqcGdeQXVyNDAzNDk0MTQ@._V1_.jpg" 
+      title="A Netfix Original Series" 
+      sname="Kota factory" 
+      link="https://www.netflix.com/in/title/81249783?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp5993832.png" 
+      title="A Netfix Original Series" 
+      sname="Money heist" 
+      link="https://www.netflix.com/in/title/80192098?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp3814512.png" 
+      title="A Netfix Original Series" 
+      sname="Sacred games" 
+      link="https://www.netflix.com/in/title/80115328?source=35" /> 
+
+
+
+<Card imgsrc="https://m.media-amazon.com/images/M/MV5BNzkxZjBjNGItMDk0MC00NzJjLTk2ZDItNTNkYWFkMDY4MzY1XkEyXkFqcGdeQXVyNDAzNDk0MTQ@._V1_.jpg" 
+      title="A Netfix Original Series" 
+      sname="Kota factory" 
+      link="https://www.netflix.com/in/title/81249783?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp5993832.png" 
+      title="A Netfix Original Series" 
+      sname="Money heist" 
+      link="https://www.netflix.com/in/title/80192098?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp3814512.png" 
+      title="A Netfix Original Series" 
+      sname="Sacred games" 
+      link="https://www.netflix.com/in/title/80115328?source=35" /> 
+
+
+<Card imgsrc="https://m.media-amazon.com/images/M/MV5BNzkxZjBjNGItMDk0MC00NzJjLTk2ZDItNTNkYWFkMDY4MzY1XkEyXkFqcGdeQXVyNDAzNDk0MTQ@._V1_.jpg" 
+      title="A Netfix Original Series" 
+      sname="Kota factory" 
+      link="https://www.netflix.com/in/title/81249783?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp5993832.png" 
+      title="A Netfix Original Series" 
+      sname="Money heist" 
+      link="https://www.netflix.com/in/title/80192098?source=35" /> 
+<Card imgsrc="https://wallpapercave.com/wp/wp3814512.png" 
+      title="A Netfix Original Series" 
+      sname="Sacred games" 
+      link="https://www.netflix.com/in/title/80115328?source=35" /> 
+
+
+</>,
+     document.getElementById("root")
+);
